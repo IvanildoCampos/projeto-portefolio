d2f3cf5c9bf58925e41eb9bc1fa0945694c45f18
Ivanildo Campos <125228040+IvanildoCampos@users.noreply.github.com>
2024-10-30 16:04:26 -0300

    atualização

--- menu.js
+++ menu.js
@@ -1,5 +1,5 @@
 let btnMenu = document.getElementById('btn_menu')
-let menu = document.getElementById('btn_mobile')//menu_mobile
+let menu = document.getElementById('menu_mobile')
 let overlay = document.getElementById('overlay_menu')
 
 btnMenu.addEventListener('click', ()=> {
