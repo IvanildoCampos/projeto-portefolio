ead20f1aad3d5ecdae707dbadbaf11579bf637ba
Ivanildo Campos <125228040+IvanildoCampos@users.noreply.github.com>
2024-10-30 22:35:10 -0300

    atualização

--- menu.js
+++ menu.js
@@ -8,6 +8,34 @@ btnMenu.addEventListener('click', ()=> {
 menu.addEventListener('click', ()=> {
     menu.classList.remove('abrir_menu')
 })
-/*overlay.addEventListener('click', ()=> {
-    menu.classList.remove('abrir_menu') 
-})*/
+
+//captura de dados do formulário e envio
+document.querySelector('form').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    
+    const formData = {
+      nome: event.target[0].value,
+      email: event.target[1].value,
+      celular: event.target[2].value,
+      mensagem: event.target[3].value,
+    };
+  
+    try {
+      const response = await fetch('/enviar-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      
+      if (response.ok) {
+        alert('E-mail enviado com sucesso!');
+      } else {
+        alert('Erro ao enviar o e-mail.');
+      }
+    }
+     catch (error) {
+      alert('Erro ao enviar o e-mail.');
+    }
+  });
