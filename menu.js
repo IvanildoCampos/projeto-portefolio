d9e43777149ed43e806ec84f2e84e551c16f9e72
Ivanildo Campos <125228040+IvanildoCampos@users.noreply.github.com>
2024-10-31 13:03:05 -0300

    atualização

--- menu.js
+++ menu.js
@@ -12,29 +12,32 @@ menu.addEventListener('click', ()=> {
 
 // Inicialize o EmailJS com seu User ID
 //https://cdn.emailjs.com/dist/email.min.js
+
 (function(){
     emailjs.init("u214o-KY6xUsJVFlk");
  })();
- 
- document.getElementById("contactForm").addEventListener("submit", function(event) {
-   event.preventDefault(); // Evita o envio padrão do formulário
- 
-   const serviceID = "service_tj0yz0q"; // Seu Service ID no EmailJS
-   const templateID = "template_a7jjnn9"; // Seu Template ID no EmailJS
- 
-   const templateParams = {
-     name: document.getElementById("name").value,
-     email: document.getElementById("email").value,
-     telefone: document.getElementById("telefone").value,
-     message: document.getElementById("message").value,
-   };
- 
-   emailjs.sendForm(serviceID, templateID, templateParams)
-     .then(response => {
-       alert("E-mail enviado com sucesso!");
-       document.getElementById("contactForm").reset(); // Limpa o formulário
-     }, error => {
-       alert("Erro ao enviar o e-mail. Tente novamente.");
-       console.log("Erro:", error);
-     });
- });
+
+ window.onload = function {
+  document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita o envio padrão do formulário
+  
+    const serviceID = "service_tj0yz0q"; // Seu Service ID no EmailJS
+    const templateID = "template_a7jjnn9"; // Seu Template ID no EmailJS
+  
+    const templateParams = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      telefone: document.getElementById("telefone").value,
+      message: document.getElementById("message").value,
+    };
+  
+    emailjs.sendForm(serviceID, templateID, templateParams)
+      .then(() => {
+        alert("E-mail enviado com sucesso!");
+        document.getElementById("contactForm").reset(); // Limpa o formulário
+      }, error => {
+        alert("Erro ao enviar o e-mail. Tente novamente.");
+        console.log("Erro:", error);
+      });
+  });
+}
