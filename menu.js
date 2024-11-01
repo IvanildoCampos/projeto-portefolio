23b76e282b7f0bb92751f3d0a2a2020059fbcd30
Ivanildo Campos <125228040+IvanildoCampos@users.noreply.github.com>
2024-11-01 11:33:55 -0300

    manunteção em form e especialidades

--- menu.js
+++ menu.js
@@ -1,7 +1,32 @@
-//abrir e fechar menu
+// Inicialize o EmailJS com seu User ID
+
+(function(){
+    emailjs.init("u214o-KY6xUsJVFlk");
+ })();
+
+ window.onload = function() {
+  document.getElementById('contactForm').addEventListener('submit', function(event) {
+      event.preventDefault(); // Evita o envio padrão do formulário
+
+      const serviceID = "service_tj0yz0q"; // Seu Service ID no EmailJS
+      const templateID = "template_a7jjnn9"; // Seu Template ID no EmailJS
+
+      /*const mensagem = document.querySelector('textarea[name="mensagem"]').value;
+        console.log("Valor do textarea:", mensagem); // Imprime o valor no console*/
+    
+      emailjs.sendForm(serviceID, templateID, this)
+          .then(() => {
+              alert("E-mail enviado com sucesso!");
+              document.getElementById("contactForm").reset(); // Limpa o formulário
+          }, (error) => {
+              alert("Erro ao enviar o e-mail. Tente novamente.");
+              console.log("Erro:", error);
+          });
+  });
+}
+  //abrir e fechar menu
 let btnMenu = document.getElementById('btn_menu')
 let menu = document.getElementById('menu_mobile')
-let overlay = document.getElementById('overlay_menu')//clicar no overlay opacidade e fechar
 
 btnMenu.addEventListener('click', ()=> {
     menu.classList.add('abrir_menu')
@@ -9,36 +34,4 @@ btnMenu.addEventListener('click', ()=> {
 menu.addEventListener('click', ()=> {
     menu.classList.remove('abrir_menu')
 })
-
-// Inicialize o EmailJS com seu User ID
-//https://cdn.emailjs.com/dist/email.min.js
-
-(function(){
-    emailjs.init("u214o-KY6xUsJVFlk");
- })();
-
- window.onload = function() {
-  document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita o envio padrão do formulário
-  
-    const serviceID = "service_tj0yz0q"; // Seu Service ID no EmailJS
-    const templateID = "template_a7jjnn9"; // Seu Template ID no EmailJS
-  
-    const templateParams = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      telefone: document.getElementById("telefone").value,
-      message: document.getElementById("message").value,
-    };
-  
-    emailjs.sendForm(serviceID, templateID, templateParams)
-      .then(() => {
-        alert("E-mail enviado com sucesso!");
-        document.getElementById("contactForm").reset(); // Limpa o formulário
-      }, error => {
-        alert("Erro ao enviar o e-mail. Tente novamente.");
-        console.log("Erro:", error);
-      });
-  });
- }
-  
+console.log(menu)
